c7729dcfad89020572a7a0dbf3998f056898726a
deepa <deepa@DESKTOP-AJQTNNR>
2022-03-14 15:35:48 +0530

    HashSet

---
+++ 01Practice/src/com/nit/hk/suncollections/Test10_HashSet.java
@@ -0,0 +1,34 @@
+package com.nit.hk.suncollections;
+
+import java.util.HashSet;
+
+public class Test10_HashSet {
+
+	public static void main(String[] args) {
+		HashSet<Object> hs = new HashSet<>();
+		System.out.println(hs.isEmpty());
+		System.out.println(hs.size());
+		System.out.println(hs);
+		System.out.println();
+		
+		hs.add("a");
+		hs.add("b");
+		hs.add(5);
+		hs.add(6.7);
+		hs.add(true);
+		hs.add(null);
+		hs.add(new A(5,6));
+		hs.add("a");
+		hs.add(5);
+		hs.add(new A(5,6));
+		hs.add(new A(7,8));
+		A a4 = new A(3,4);
+		hs.add(a4);
+		hs.add(a4);
+		
+		System.out.println(hs);
+		System.out.println(hs.size());
+		System.out.println(hs.isEmpty());
+	}
+
+}
