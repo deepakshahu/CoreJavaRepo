0db47388f5ce9c9cb4ea06715a80570f0967963b
deepakshahu <deepakshahu1996@gmail.com>
2022-05-09 13:08:53 +0530

    TreeSet

--- 01Practice/src/com/nit/hk/suncollections/Test15_TreeSet.java
+++ 01Practice/src/com/nit/hk/suncollections/Test15_TreeSet.java
@@ -8,7 +8,7 @@ public class Test15_TreeSet {
 		ts1.add("a");
 		ts1.add("c");  //"c".compareTo("a") -> +ve -> RIGHT SIDE
 		ts1.add("b");  //"b".compareTo("a") -> +ve -> RIGHT SIDE
-				       //"b".compareTo("c") -> -ve -> LEFT SIDE
+		//"b".compareTo("c") -> -ve -> LEFT SIDE
 		System.out.println(ts1);
 
 		//ts.add(5);
@@ -24,44 +24,80 @@ public class Test15_TreeSet {
 		ts3.add(new B(5,9));  //b1.compareTo(b1) //ignored
 		ts3.add(new B(7,2));  //b2.compareTo(b1) -> +ve -> RS
 		ts3.add(new B(6,4));  //b3.compareTo(b1) -> +ve -> RS
-		   					  //b3.compareTo(b2) -> -ve -> LS
+		//b3.compareTo(b2) -> -ve -> LS
 		ts3.add(new B(6,4));
 		System.out.println(ts3);
-		
+
 		TreeSet<B> ts4 = new TreeSet<>(new BxDescComparator());
 		ts4.add(new B(5,9));  
 		ts4.add(new B(7,2));  //cmpr.compare(b2,b1) -> -ve -> LS
 		ts4.add(new B(6,4));  //cmpr.compare(b3,b1) -> -ve -> LS
-		   					  //cmpr.compare(b3,b2) -> +ve -> RS
+		//cmpr.compare(b3,b2) -> +ve -> RS
 		ts4.add(new B(6,4));
 		System.out.println(ts4);
-		
+
 		//TS Sorting algorithm has two logics
-			//1.comparing logic -> developer
-			//2.swapping logic -> SUN
-		
 		//1.comparing logic -> developer
-			//by implementing comparable interface method compareTo() in the same adding object class
-				//1) return -ve to tell smaller object
-				//2) return +ve to tell bigger object
-				//3) return 0 to tell duplicate object
-		
 		//2.swapping logic -> SUN
-			//1) if compareTo() returns -ve -> CO is stored LEFT to TS element
-			//2) if compareTo() returns +ve -> CO is stored RIGHT to TS element
-			//3) if compareTo() returns 0 -> CO is not stored
-		
+
+		//1.comparing logic -> developer
+		//by implementing comparable interface method compareTo() in the same adding object class
+		//1) return -ve to tell smaller object
+		//2) return +ve to tell bigger object
+		//3) return 0 to tell duplicate object
+
+		//2.swapping logic -> SUN
+		//1) if compareTo() returns -ve -> CO is stored LEFT to TS element
+		//2) if compareTo() returns +ve -> CO is stored RIGHT to TS element
+		//3) if compareTo() returns 0 -> CO is not stored
+
 		//TS does not have default sorting order
 		//TS default sorting order is not ascending order
 		//TS sort elements according to the natural sorting order
 		//given in adding object class comparable interface compareTo()
-		
+
 		//In compareTo method we can place below 5 cases logic
-		
+
 		//If we place CO - AO --> objects are stored in Ascending order
 		//If we place AO - CO --> objects are stored in descending order
 		//If we place +ve num --> objects are stored in Insertion order including duplicates
 		//If we place -ve num --> objects are stored in reverse insertion order including duplicates
 		//If we place 0 ==> only first object is stored
+
+		//For Storing objects in TS
+		//1)the object must be either Comparable(TS)
+		//2)we must apply custom comparator
+
+		TreeSet<C> ts5 = new TreeSet<>((c1,c2)->c1.getI() - c2.getI());
+		ts5.add(new C(5, 8.4, "c"));
+		ts5.add(new C(7, 8.3, "b"));
+		ts5.add(new C(6, 8.2, "a"));
+		System.out.println(ts5);
+
+		TreeSet<C> ts6 = new TreeSet<>((c1,c2)-> (int)(c1.getD() - c2.getD()));
+		ts6.add(new C(5, 8.4, "c"));
+		ts6.add(new C(7, 8.3, "b"));
+		ts6.add(new C(6, 8.2, "a"));
+		System.out.println(ts6);
+
+		TreeSet<C> ts7 = new TreeSet<>((c1,c2) -> {
+			double diff = c1.getD() - c2.getD();
+			if(diff < 0)
+				return -1;
+			else if(diff > 0)
+				return 1;
+			else
+				return 0;
+		});
+		ts7.add(new C(5, 8.4, "b"));
+		ts7.add(new C(7, 8.3, "c"));
+		ts7.add(new C(6, 8.2, "a"));
+		System.out.println(ts7);
+
+		TreeSet<C> ts8 = new TreeSet<>((c1,c2) -> c1.getS().hashCode() - c2.getS().hashCode());
+		ts8.add(new C(5, 8.4, "b"));
+		ts8.add(new C(7, 8.3, "c"));
+		ts8.add(new C(6, 8.2, "a"));
+		System.out.println(ts8);
 	}
 }
