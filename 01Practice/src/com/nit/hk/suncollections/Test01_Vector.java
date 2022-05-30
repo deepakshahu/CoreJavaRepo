dd32d48a0c64e4ae1660110b792feeccff35b3e6
deepakshahu <deepakshahu1996@gmail.com>
2022-05-30 23:34:29 +0530

    Vector

--- 01Practice/src/com/nit/hk/suncollections/Test01_Vector.java
+++ 01Practice/src/com/nit/hk/suncollections/Test01_Vector.java
@@ -1,56 +1,58 @@
 package com.nit.hk.suncollections;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Vector;
 
 public class Test01_Vector {
 	public static void main(String[] args) {
-		/*		Vector<Integer> v1 = new Vector<>();
-				v1.add(5);
-				v1.add(7);
-				v1.add(9);
-				v1.add(5);
-				v1.add(7);
-				System.out.println(v1);
+		/*
+		Vector<Integer> v1 = new Vector<>();
+		v1.add(5);
+		v1.add(7);
+		v1.add(9);
+		v1.add(5);
+		v1.add(7);
+		System.out.println(v1);
 
-				Vector<Integer> v2 = new Vector<>();
-				v2.add(25);
-				v2.addAll(v1);
-				v2.add(35);
-				System.out.println(v2);
+		Vector<Integer> v2 = new Vector<>();
+		v2.add(25);
+		v2.addAll(v1);
+		v2.add(35);
+		System.out.println(v2);
 
-				Vector<String> v3 = new Vector<>();
-				// v3.addAll(v1);
+		Vector<String> v3 = new Vector<>();
+		// v3.addAll(v1);
 
-				System.out.println(v1.contains(5));
-				System.out.println(v1.contains(8));
-				System.out.println(v2.contains(v1));
-				System.out.println(v2.containsAll(v1));
-				System.out.println();
-
-				System.out.println(v1);
-				System.out.println(v1.remove(9));
-				System.out.println(v1);
-
-				System.out.println(v2);
-				//System.out.println(v2.removeAll(v1));
-				System.out.println(v2.retainAll(v1));
-				System.out.println(v2);
-
-				System.out.println(v1.size());
-				System.out.println(v2.size());
-
-				v1.clear();
-				System.out.println(v1);
-
-				Object[] obj = v2.toArray();
-				System.out.println(obj.length);
-				System.out.println(Arrays.toString(obj));
-
-				Integer[] ia = new Integer[v2.size()];
-				v2.toArray(ia);
-				System.out.println(Arrays.toString(ia));*/
-
+		System.out.println(v1.contains(5));
+		System.out.println(v1.contains(8));
+		System.out.println(v2.contains(v1));
+		System.out.println(v2.containsAll(v1));
+		System.out.println();
+		
+		System.out.println(v1);
+		System.out.println(v1.remove(1));
+		System.out.println(v1);
+		
+		System.out.println(v2);
+		//System.out.println(v2.removeAll(v1));
+		System.out.println(v2.retainAll(v1));
+		System.out.println(v2);
+		
+		System.out.println(v1.size());
+		System.out.println(v2.size());
+		
+		v1.clear();
+		System.out.println(v1);
+		
+		Object[] obj = v2.toArray();
+		System.out.println(obj.length);
+		System.out.println(Arrays.toString(obj));
+		
+		Integer[] ia = new Integer[v2.size()];
+		v2.toArray(ia);
+		System.out.println(Arrays.toString(ia));
+		*/
 		Vector<Object> v1 = new Vector<>();
 		System.out.println(v1.capacity());
 		System.out.println(v1.size());
@@ -203,5 +205,6 @@ public class Test01_Vector {
 		v8.add(3);
 		v8.add(4);
 		System.out.println(v8.capacity());
+		
 	}
 }
